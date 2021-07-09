3d05897185484c216b875f09aa41f9c6a6b6c784
dreamCode <59535704+np03a170120@users.noreply.github.com>
2021-07-09 19:20:04 +0545

    First draft homepage

--- footer.php
+++ footer.php
@@ -1,3 +1,61 @@
+<footer>
+    <section class="footer-main">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 footer-link-pad">
+                    <div class="opening-hours">
+                        <div class="footer-title">
+                            <h3>Opening Hours</h3>
+                        </div>
+                        <ul class="oh-content">
+                            <li>Summer 9:30 AM - 5:30 AM</li>
+                            <li>Winter 9:30 AM - 5:30 AM</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 footer-link-pad">
+                    <div class="quick-links">
+                        <div class="footer-title">
+                            <h3>Quick Links</h3>
+                        </div>
+                        <ul class="oh-content">
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">Notices</a></li>
+                            <li><a href="#">Gallery</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 footer-link-pad">
+                    <div class="quick-links">
+                        <div class="footer-title">
+                            <h3>Contact Us</h3>
+                        </div>
+                        <ul class="oh-content">
+                            <li><a href="#"><i class="fas fa-map-marker-alt"></i>Maitighar, Kathmandu</a></li>
+                            <li><a href="#"><i class="fas fa-phone-square-alt"></i>01-4486712</a></li>
+                            <li><a href="#"><i class="fas fa-envelope"></i>company@email.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="footer-bottom">
+        <div class="container">
+            <div class="ft-content j-c-s">
+                <div class="ft-left">
+                    <span>© Copyright Central Law Library. All Rights Reserved
+                    </span>
+                </div>
+                <div class="ft-right">
+                    <i class="fab fa-facebook-square"></i>
+                    <i class="fab fa-twitter-square"></i>
+                </div>
+            </div>
+        </div>
+    </section>
+</footer>
 </body>
 <script src="./js/jquery-3.6.0.min.js"></script>
 <script src="./js/bootstrap.min.js"></script>
